c8d17c1dc4e73c92ed49101633511a07cd953508
yattinda <yattinda@gmail.com>
2019-10-17 19:17:40 +0900

    a

--- application/server.py
+++ application/server.py
@@ -1,6 +1,4 @@
-#server.py
-
 from flask_blog import app
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug=True)
--- apptest/flask_blog/__init__.py
+++
@@ -1,8 +0,0 @@
-# flask_blog/__init__.py
-
-from flask import Flask
-
-app = Flask(__name__)
-
-import flask_blog.views
-
