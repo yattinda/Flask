2fc0ee0daec2912a9c2549607de16518f83777f9
yattinda <yattinda@gmail.com>
2019-10-17 19:44:22 +0900

    m

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -1,6 +1,8 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from jinja2 import FileSystemLoader
 
+# app.jinja_loader = FileSystemLoader("templates/entries")
 @app.route('/')
 def show_entries():
-    return render_template("flask_blog/template/entries/index.html")
+    return render_template("index.html")
