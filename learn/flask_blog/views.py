a3fb252a28461f53814c47752cd3fefc213b5b4b
yattinda <yattinda@gmail.com>
2020-03-10 18:29:50 +0900

    first commit

---
+++ learn/flask_blog/views.py
@@ -0,0 +1,6 @@
+from flask_blog import app
+from flask import request, redirect, url_for, render_template, flash, session
+
+@app.route("/")
+def show_entries():
+    return render_template("entries/index.html")
